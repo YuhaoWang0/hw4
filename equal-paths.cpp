5d58f3fe699c6a51a659726c600b5a24aad5b83e
Yuhao Wang <ywang755@usc.edu>
2025-11-12 02:07:21 +0000

    Complete equal paths problem

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,40 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+bool toLeaves(Node* root, int depth, int& leaveDepth) {
+  if(root == nullptr) {
+    return true;
+  }
 
+  if(root->left == nullptr && root->right == nullptr) {
+    
+    if(leaveDepth == -1) {
+      leaveDepth = depth;
+      return true;
+    }
+
+    if(depth == leaveDepth) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool left = toLeaves(root->left, depth + 1, leaveDepth);
+  bool right = toLeaves(root->right, depth + 1, leaveDepth);
+
+  if(left && right) {
+    return true;
+  } 
+
+  return false;
+}
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
 
+  // Add your code below
+  int initialValue = -1;
+  return toLeaves(root, 0, initialValue);
 }
 
